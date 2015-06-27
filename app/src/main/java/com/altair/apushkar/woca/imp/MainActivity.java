2f0a5fd438783c96f937bc5d6e42308050b22d19
Alexander <steelsoul@ukr.net>
2015-06-27 16:45:04 +0300

    Added menu.

--- app/src/main/java/com/altair/apushkar/woca/imp/MainActivity.java
+++ app/src/main/java/com/altair/apushkar/woca/imp/MainActivity.java
@@ -2,21 +2,23 @@
 package com.altair.apushkar.woca.imp;
 
 import android.app.Activity;
-import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.altair.apushkar.woca.R;
 import com.altair.apushkar.woca.api.ILanguageDB;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
     private final String LOG_TAG = "[MainActivity]";
     private final String DB_NAME = "a5.db";
 
@@ -65,5 +67,20 @@ public class MainActivity extends Activity {
         esDirection.setAdapter(adapter);
 
         Log.d(LOG_TAG, "Created activity");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        Log.d(LOG_TAG, "Create menu");
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
